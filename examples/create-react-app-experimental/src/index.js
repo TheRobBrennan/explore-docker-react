fdeec293fa61d1d8b2549103c42498a4ae344046
Rob Brennan <rob@therobbrennan.com>
2019-12-06 13:32:22 -0800

    Add concurrent mode in our main index.js

--- examples/create-react-app-experimental/src/index.js
+++ examples/create-react-app-experimental/src/index.js
@@ -4,7 +4,13 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Current release of React (which does not have support for concurrent mode)
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// If we have react@experimenal and react-dom@experimental, we can enable concurrent mode
+const rootEl = document.getElementById('root');
+const root = ReactDOM.createRoot(rootEl);
+root.render(<App />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
